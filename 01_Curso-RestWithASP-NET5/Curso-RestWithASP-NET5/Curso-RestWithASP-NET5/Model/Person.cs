d4f2b72855da47da604dafca8a532536ac5576d4
Robson de Moraes Silva <rms2307@outlook.com>
2021-01-04 18:08:13 -0300

    model person

---
+++ 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_RestWithASP_NET5.Model
+{
+    public class Person
+    {
+    }
+}
