e792035726dfc9bd02f891374eff348db78d412b
Robson de Moraes Silva <rms2307@outlook.com>
2021-01-04 18:15:42 -0300

    interface IPersonService

---
+++ 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using Curso_RestWithASP_NET5.Model;
+using System.Collections.Generic;
+
+namespace Curso_RestWithASP_NET5.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByIs(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
