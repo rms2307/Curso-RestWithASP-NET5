62c3e9d4a016b345ed505c2807eeae42f2e33666
Robson de Moraes Silva <rms2307@outlook.com>
2021-01-04 17:45:33 -0300

    outras operacoes

--- 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Controllers/CalculatorController.cs
+++ 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Controllers/CalculatorController.cs
@@ -19,12 +19,76 @@ namespace Curso_RestWithASP_NET5.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult GetSum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString());
+            }
+
+            return BadRequest("Entrada Invalida");
+        }
+
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+
+            return BadRequest("Entrada Invalida");
+        }
+
+        [HttpGet("multi/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMulti(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multi = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multi.ToString());
+            }
+
+            return BadRequest("Entrada Invalida");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult GetDiv(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(secondNumber) == 0)
+                {
+                    return BadRequest("Erro: Divisão por 0");
+                }
+                var div = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(div.ToString());
+            }
+
+            return BadRequest("Entrada Invalida");
+        }
+
+        [HttpGet("media/{firstNumber}/{secondNumber}")]
+        public IActionResult GetMedia(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var media = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(media.ToString());
+            }
+
+            return BadRequest("Entrada Invalida");
+        }
+
+        [HttpGet("sqtr/{number}")]
+        public IActionResult GetSqtr(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var sqtr = System.Math.Sqrt(double.Parse(number));
+                return Ok(sqtr.ToString());
             }
 
             return BadRequest("Entrada Invalida");
