882f6c7d88a827ae21dbc57cfd3cab5d8b9cbe72
Robson de Moraes Silva <rms2307@outlook.com>
2021-01-08 17:33:39 -0300

    Update PersonVO.cs

--- 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Data/VO/PersonVO.cs
+++ 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Data/VO/PersonVO.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Curso_RestWithASP_NET5.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("Codigo")]
         public long Id { get; set; }
 
+        [JsonPropertyName("NomeCompleto")]
         public string FullName { get; set; }
-        
+
+        [JsonIgnore]
         public string Address { get; set; }
 
-        
+        [JsonPropertyName("Sexo")]
         public string Gender { get; set; }
     }
 }
