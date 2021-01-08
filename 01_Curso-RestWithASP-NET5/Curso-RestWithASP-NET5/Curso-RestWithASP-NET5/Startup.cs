fcdd02aa4cdfeaa962602ee9072ecf6673e363f5
Robson de Moraes Silva <rms2307@outlook.com>
2021-01-08 19:17:50 -0300

    implementando cors

--- 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Startup.cs
+++ 01_Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Curso-RestWithASP-NET5/Startup.cs
@@ -40,6 +40,11 @@ namespace Curso_RestWithASP_NET5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddDefaultPolicy(builder => 
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            }));
+
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
@@ -93,6 +98,8 @@ namespace Curso_RestWithASP_NET5
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
